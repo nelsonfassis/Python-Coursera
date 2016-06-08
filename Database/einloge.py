4f1beeabced803986d97bf1082d913efc1dae75b
nelsonfassis <nelson.f.assis@gmail.com>
2016-06-08 13:09:18 -0700

    Einloge improvment

--- Database/einloge.py
+++ Database/einloge.py
@@ -3,23 +3,63 @@ import sqlite3
 conn = sqlite3.connect("einloge.sqlite")
 cur = conn.cursor()
 
-cur.execute('''
-DROP TABLE IF EXISTS Clients''')
+def add_client():
+	print "To break the loop, do not insert any value into the Client name"
+	while True:
+		client_name = raw_input("Insert client name: ")
+		if len(client_name) <= 1 : break
+		client_cpf = input("Inser client CPF: ")
+		client_company = raw_input("Insert client's company name: ").lower()
+		print client_company
+		cur.execute('''INSERT OR IGNORE INTO Clients(name, cpf, company_id) values (?,?,?)''',(client_name,client_cpf,client_company))
+		conn.commit()
+		cur.close()
 
-cur.execute('''
-CREATE TABLE Clients
+def add_company():
+	print "To break the loop, do not insert any value into the Company name"
+	while True:
+		company_name = raw_input("Insert company name: ")
+		if len(company_name) <= 1 : break
+		company_cnpj = input("Inser CNPJ: ")
+		cur.execute('''INSERT OR IGNORE INTO Company(name, cnpj) values (?,?)''',(company_name,cnpj))
+		conn.commit()
+
+def whatodo():
+	print ("What would you like to do?")
+	print ("1 - Register a company \n2 - Register a user\n 3 - Show users")
+	try : decision = int(raw_input("I would like to: "))
+	except: print ("Insert a numeric value")
+	if decision == 1:
+		add_company()
+	if decision == 2:
+		add_client()
+	#else if decision == 3:
+	#	show_users()
+	else: print("Invalid input")
+			
+	
+
+#cur.close()
+#cur.execute('''
+#DROP TABLE IF EXISTS Clients''')
+
+#cur.execute('''
+#DROP TABLE IF EXISTS Company''')
+
+cur.executescript('''
+CREATE IF NOT EXISTS TABLE Clients
 (id INTENGER PRIMARY KEY NOT NULL,
 name TEXT, 
 cpf INTEGER,
-company_id
-)''')
+company_id);
 
-print "To break the loop, do not insert any value into the Client name"
-while True:
-	client_name = raw_input("Insert client name: ")
-	if client_name <= 1: break
-	client_cpf = input("Inser client CPF: ")
-	client_company = raw_input("Insert client's company name: ").lower()
-	print client_company
+CREATE IF NOT EXISTS TABLE Company
+(id INTENGER PRIMARY KEY NOT NULL,
+name TEXT, 
+cnpj INTEGER);
+''')
 
-cur.commit()
+whatodo()
+
+result = cur.execute('''SELECT name,cpf FROM Clients''')
+print str(result)
